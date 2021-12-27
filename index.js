72a005fb306aa5835f92de5c478c8e3b695b4fd7
Taranjeet105 <shantys502@gmail.com>
2021-12-27 19:35:24 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const express=require('express')
 require('dotenv').config()
 const path=require('path')
-// const cookieParser=require('cookie-parser')
+
 const bodyParser=require('body-parser')
-// const mongoose=require('mongoose')
-// const {mongoUrl}=require('./config/keys')
-// const cors = require('cors');
-// const multer=require('multer')
+
 const app=express()
 const compiler=require('./middleware/compiler')
-const { promise } = require('bcrypt/promises')
 const PORT= process.env.PORT || 5000
-// app.use(cookieParser())
-// require('./models/User')  // routes will come after this, because we are using models in routes
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
@@ -22,16 +17,6 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.set('view engine','ejs')
 
-// mongoose.connect(mongoUrl,{
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true,
-//     useCreateIndex:true
-
-// }).then(()=>{
-//     console.log("succes")
-// }).catch((e)=>{
-//     console.log(e)
-// })
 app.get('/',(req,res)=>{
     res.json({message:"home page"})
  
@@ -46,7 +31,7 @@ app.post('/compile',async (req,res)=>{
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('my-app/build'))
     app.get("*",(req,res)=>{
-        res.sendFile(__dirname,'my-app','build','index.html')
+        res.sendFile(path.resolve(__dirname,'my-app','build','index.html'))
     })
 }else{
 
